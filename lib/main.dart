71960f8761407736b48955f0f213e041433699a8
FrostedFlakes765 <frost.the.captain@gmail.com>
2022-10-14 09:51:47 +0300

    New Login platform

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'widget_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WidgetTree(),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:vid_app/splash_screen.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData.dark(),
+//       home: const Splash(),
+//     );
+//   }
+// }
